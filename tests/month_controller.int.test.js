cb0d5422cc37bf346176a0dd01aeccf27e85f8d5
Ian <mus5002@aol.com>
2021-04-28 18:10:00 -0400

    moved dev dependencies, started writing test file for the controller

---
+++ tests/month_controller.int.test.js
@@ -0,0 +1,9 @@
+const req = require('supertest')
+const server = require('../../index')
+const newMonth = require('../mock-data/new-month.json')
+
+const endpointUrl = '/months/'
+
+describe(endpointUrl, () => {
+    
+})
