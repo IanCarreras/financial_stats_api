9ccc6e9acefc3d9c14eb2b5a329a18c9608ed1c1
Ian <mus5002@aol.com>
2021-05-18 19:15:59 -0400

    added test for model.create

--- tests/month_controller.int.test.js
+++ tests/month_controller.int.test.js
@@ -1,16 +1,23 @@
 const req = require('supertest')
 const server = require('../../index')
 const newMonth = require('../mock-data/new-month.json')
+const MonthController = require('../controllers/month_controller')
+const MonthModel = require('../models/month_model')
 
 const endpointUrl = '/months/'
 
 describe(endpointUrl, () => {
-    it('POST' + endpointUrl, async () => {
-        const res = await req(server)
-            .post(endpointUrl)
-            .send(newMonth)
-        expect(res.statusCode).toBe(201)
-        expext(res.body.month).toBe(newMonth.month)
-        firstMonth = res.body._id
+    it('should call MonthModel.create', () => {
+        MonthController.createMonth()
+        expect(MonthModel.create).toBeCalled()
     })
+
+    // it('POST' + endpointUrl, async () => {
+    //     const res = await req(server)
+    //         .post(endpointUrl)
+    //         .send(newMonth)
+    //     expect(res.statusCode).toBe(201)
+    //     expext(res.body.month).toBe(newMonth.month)
+    //     firstMonth = res.body._id
+    // })
 })
